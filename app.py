fec91e15909fd0bff67487ef6d5ee4854446e961
DadvaiahPavan <128118293+DadvaiahPavan@users.noreply.github.com>
2025-08-07 17:11:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1065,8 +1065,8 @@ def logout():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        print("Database tables created successfully!")
     port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port)
+
 
 
